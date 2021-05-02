b9cf2f5292c5f66fd8c823231a6aab0c87e6dcc4
Therainisme <me@Therainisme.com>
2021-05-02 15:55:21 +0800

    Therainisme added vuepress pages

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -19,55 +19,58 @@ module.exports = {
     themeConfig: {
         // 添加导航栏
         nav: [{
-                text: 'GitHub',
-                // 这里是下拉列表展现形式。
-                link: 'https://github.com/VisualDust/ml.akasaki.space'
-            }
-        ],
+            text: 'GitHub',
+            // 这里是下拉列表展现形式。
+            link: 'https://github.com/VisualDust/ml.akasaki.space'
+        }],
         // 为以下路由添加侧边栏
         sidebar: {
-            '/' : [
+            '/': [
                 {
-                title: '第零章：在开始之前',
-                children: getSidebar('ch0')
-            },
-            {
-                title: '第一章上：这HelloWorld有点长啊',
-                children: getSidebar('ch1p1')
-            },
-            {
-                title: '第一章下：深度学习基础——多层感知机',
-                children: getSidebar('ch1p2')
-            },
-            {
-                title: '第二章上：卷积神经网络及其要素',
-                children: getSidebar('ch2p1')
-            },
-            {
-                title: '第二章下：经典卷积神经网络',
-                children: getSidebar('ch2p2')
-            },
-            {
-                title: '第三章上：谈一些计算机视觉的方向',
-                children: getSidebar('ch3p1')
-            },
-            {
-                title: '第三章下：尝试一些计算机视觉任务',
-                children: getSidebar('ch3p2')
-            },
-            {
-                title: '附录',
-                children: getSidebar('appendix')
-            },
-            {
-                title: '第-1章：TensorFlow编程策略',
-                children: getSidebar('ch-1')
-            },
-            {
-                title: '第-2章：数字信号处理（DSP）',
-                children: getSidebar('ch-2')
-            },
-        ]
+                    title: '第零章：在开始之前',
+                    children: getSidebar('ch0')
+                },
+                {
+                    title: '第一章上：这HelloWorld有点长啊',
+                    children: getSidebar('ch1p1')
+                },
+                {
+                    title: '第一章下：深度学习基础——多层感知机',
+                    children: getSidebar('ch1p2')
+                },
+                {
+                    title: '第二章上：卷积神经网络及其要素',
+                    children: getSidebar('ch2p1')
+                },
+                {
+                    title: '第二章下：经典卷积神经网络',
+                    children: getSidebar('ch2p2')
+                },
+                {
+                    title: '第三章上：谈一些计算机视觉的方向',
+                    children: getSidebar('ch3p1')
+                },
+                {
+                    title: '第三章下：尝试一些计算机视觉任务',
+                    children: getSidebar('ch3p2')
+                },
+                {
+                    title: '附录',
+                    children: getSidebar('appendix')
+                },
+                {
+                    title: '第-1章：TensorFlow编程策略',
+                    children: getSidebar('ch-1')
+                },
+                {
+                    title: '第-2章：数字信号处理（DSP）',
+                    children: getSidebar('ch-2')
+                },
+                {
+                    title: '无尽模式',
+                    children: getSidebar('unlimited-paper-works')
+                },
+            ]
         }
     },
     plugins: [
