03d9443f00f434c1a095ee2d3f8d4603e6effaf1
VisualDust <Miya@Akasaki.space>
2021-05-02 16:22:00 +0800

    whatever

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deployed'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/visualDust/ml.akasaki.space.git master:vuepress_pages
+git push -f https://github.yuuza.net/visualDust/ml.akasaki.space.git master:vuepress_pages
 
 cd -
