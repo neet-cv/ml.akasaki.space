8f23d0c352fba0ed49152d187b3b9dbdb0d16ed7
lideming <me@yuuza.net>
2021-10-29 04:29:28 +0800

    use esbuild-loader for faster building

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,6 +47,17 @@ const config = {
     ],
   ],
 
+  webpack: {
+    jsLoader: (isServer) => ({
+      loader: require.resolve('esbuild-loader'),
+      options: {
+        loader: 'tsx',
+        format: isServer ? 'cjs' : undefined,
+        target: isServer ? 'node12' : 'es2017',
+      },
+    }),
+  },
+
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
