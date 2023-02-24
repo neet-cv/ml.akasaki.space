752e3dc310b09a42f4df95f79bc2f76c59fb5269
李得铭 <lideming.him@bytedance.com>
2023-02-24 16:48:42 +0800

    Upgrade deps; remove nodejieba

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -68,17 +68,6 @@ const config = {
         docsRouteBasePath:"/",
         docsDir:"docs",
         // ```
-        // When applying `zh` in language, please install `nodejieba` in your project.
-        translations:{
-          "search_placeholder": "Search",
-          "see_all_results": "See all results",
-          "no_results": "No results.",
-          "search_results_for": "Search results for \"{{ keyword }}\"",
-          "search_the_documentation": "Search the documentation",
-          "count_documents_found": "{{ count }} document found",
-          "count_documents_found_plural": "{{ count }} documents found",
-          "no_documents_were_found": "No documents were found"
-        }
       },
     ],
   ],
